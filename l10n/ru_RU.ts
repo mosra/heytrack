5b1811aa3b4428150a4f75c4d181ad30a4e2cd57
Vladimír Vondruš <mosra@centrum.cz>
2010-09-25 12:11:39 +0200

    Russian translation (thanks to TI_Eugene).

---
+++ l10n/ru_RU.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU" sourcelanguage="en_US">
+<context>
+    <name>HeyTrack::Core::GenreModel</name>
+    <message>
+        <location filename="../src/Core/GenreModel.cpp" line="25"/>
+        <source>All genres</source>
+        <translation>Все жанры</translation>
+    </message>
+</context>
+<context>
+    <name>HeyTrack::Plasmoid::HeyTrack</name>
+    <message>
+        <location filename="../src/Plasmoid/HeyTrack.cpp" line="60"/>
+        <source>Required settings not available</source>
+        <translation>Необходимые настройки недоступны</translation>
+    </message>
+    <message>
+        <location filename="../src/Plasmoid/HeyTrack.cpp" line="66"/>
+        <source>Initialization...</source>
+        <translation>Инициализация...</translation>
+    </message>
+    <message>
+        <location filename="../src/Plasmoid/HeyTrack.cpp" line="92"/>
+        <source>Server error: %0</source>
+        <translation>Ошибка сервера: %0</translation>
+    </message>
+</context>
+<context>
+    <name>HeyTrack::Settings::SettingsDialog</name>
+    <message>
+        <location filename="../src/Settings/SettingsDialog.cpp" line="37"/>
+        <source>HeyTrack settings</source>
+        <translation>Настройки HeyTrack</translation>
+    </message>
+    <message>
+        <location filename="../src/Settings/SettingsDialog.cpp" line="70"/>
+        <source>Server:</source>
+        <translation>Сервер:</translation>
+    </message>
+    <message>
+        <location filename="../src/Settings/SettingsDialog.cpp" line="72"/>
+        <source>Player:</source>
+        <translation>Плеер:</translation>
+    </message>
+    <message>
+        <location filename="../src/Settings/SettingsDialog.cpp" line="74"/>
+        <source>Genre:</source>
+        <translation>Жанр:</translation>
+    </message>
+    <message>
+        <location filename="../src/Settings/SettingsDialog.cpp" line="76"/>
+        <source>Station:</source>
+        <translation>Станция:</translation>
+    </message>
+    <message>
+        <location filename="../src/Settings/SettingsDialog.cpp" line="78"/>
+        <source>Stream format:</source>
+        <translation>Формат потока:</translation>
+    </message>
+    <message>
+        <location filename="../src/Settings/SettingsDialog.cpp" line="170"/>
+        <source>Server error</source>
+        <translation>Ошибка сервера</translation>
+    </message>
+    <message>
+        <location filename="../src/Settings/SettingsDialog.cpp" line="171"/>
+        <source>An error occured during parsing server response:</source>
+        <translation>Произошла ошибка при обработке ответа сервера:</translation>
+    </message>
+</context>
+<context>
+    <name>HeyTrack::Tray::HeyTrack</name>
+    <message>
+        <location filename="../src/Tray/HeyTrack.cpp" line="41"/>
+        <source>Initialization...</source>
+        <translation>Инициализация...</translation>
+    </message>
+    <message>
+        <location filename="../src/Tray/HeyTrack.cpp" line="42"/>
+        <source>Open settings</source>
+        <translation>Открыть настройки</translation>
+    </message>
+    <message>
+        <location filename="../src/Tray/HeyTrack.cpp" line="65"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../src/Tray/HeyTrack.cpp" line="68"/>
+        <source>Play stream in player</source>
+        <translation>Играть поток плеером</translation>
+    </message>
+    <message>
+        <location filename="../src/Tray/HeyTrack.cpp" line="71"/>
+        <source>Stop player</source>
+        <translation>Остановить плеер</translation>
+    </message>
+    <message>
+        <location filename="../src/Tray/HeyTrack.cpp" line="74"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="../src/Tray/HeyTrack.cpp" line="104"/>
+        <location filename="../src/Tray/HeyTrack.cpp" line="129"/>
+        <source>%0: now playing</source>
+        <translation>%0: сейчас играет</translation>
+    </message>
+    <message>
+        <location filename="../src/Tray/HeyTrack.cpp" line="131"/>
+        <source>%0: </source>
+        <translation>%0: </translation>
+    </message>
+    <message>
+        <location filename="../src/Tray/HeyTrack.cpp" line="139"/>
+        <source>Server error</source>
+        <translation>Ошибка сервера</translation>
+    </message>
+</context>
+</TS>
