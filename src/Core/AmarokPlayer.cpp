e17653d23aaebc5ea74ac77b73c081a46c72a13d
Jan Dupal <dupal.j@seznam.cz>
2010-09-26 11:56:16 +0200

    Amarok player with asx parser
    Current version of amarok doesn't support adding asx files to tracklist
    via dbus. Simple asx parserwriten inside AmarokPlayer.cpp .

---
+++ src/Core/AmarokPlayer.cpp
@@ -0,0 +1,63 @@
+/*
+    Copyright © 2010 Jan Dupal <dupal.j@seznam.cz>
+
+    This file is part of HeyTrack.
+
+    HeyTrack is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License version 3
+    only, as published by the Free Software Foundation.
+
+    HeyTrack is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License version 3 for more details.
+*/
+
+#include "AmarokPlayer.h"
+
+#include <QtCore/QUrl>
+#include <QtCore/QRegExp>
+#include <QtNetwork/QNetworkReply>
+#include <QtDBus/QDBusInterface>
+
+namespace HeyTrack { namespace Core {
+
+AmarokPlayer::AmarokPlayer(QObject* parent): AbstractPlayer(parent), manager(new QNetworkAccessManager(this)) {
+    playerInterface = new QDBusInterface("org.kde.amarok", "/Player", "", QDBusConnection::sessionBus(), this);
+    tracklistInterface = new QDBusInterface("org.kde.amarok", "/TrackList", "", QDBusConnection::sessionBus(), this);
+}
+
+void AmarokPlayer::play(const QString& url) {
+    QRegExp rxAsx("\\.asx$");
+
+    if(rxAsx.indexIn(url) == -1) {
+        /* In case url is not .asx playlist */
+        tracklistInterface->call("AddTrack", url, true);
+    } else {
+        /* Parse asx playlist */
+        QNetworkReply* reply = manager->get(QNetworkRequest(QUrl(url)));
+
+        connect(reply, SIGNAL(finished()), SLOT(processPlaylist()));
+    }
+}
+
+void AmarokPlayer::processPlaylist()
+{
+    QNetworkReply* reply = qobject_cast<QNetworkReply*>(sender());
+    if(!reply) return;
+
+    QRegExp rxUrl("<ref href\\s?=\\s?\"(.+)\"", Qt::CaseInsensitive);
+    rxUrl.setMinimal(true);
+
+    if(rxUrl.indexIn(QString::fromLatin1(reply->readAll())) != -1) {
+        QString url = rxUrl.cap(1);
+        tracklistInterface->call("AddTrack", url, true);
+    }
+    reply->deleteLater();
+}
+
+void AmarokPlayer::stop() {
+    playerInterface->call("Stop");
+}
+
+}}
---
+++ src/Core/AmarokPlayer.h
@@ -0,0 +1,53 @@
+#ifndef HeyTrack_Core_AmarokPlayer_h
+#define HeyTrack_Core_AmarokPlayer_h
+/*
+    Copyright © 2010 Jan Dupal <dupal.j@seznam.cz>
+
+    This file is part of HeyTrack.
+
+    HeyTrack is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License version 3
+    only, as published by the Free Software Foundation.
+
+    HeyTrack is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Lesser General Public License version 3 for more details.
+*/
+
+/** @file
+ * @brief Class HeyTrack::Core::AmarokPlayer
+ */
+
+#include "AbstractPlayer.h"
+
+class QDBusInterface;
+class QNetworkAccessManager;
+
+namespace HeyTrack { namespace Core {
+
+class AmarokPlayer: public AbstractPlayer {
+    Q_OBJECT
+
+    PLAYER_DEFINE(AmarokPlayer)
+
+    protected:
+        /** @brief Network access manager */
+        QNetworkAccessManager* manager;
+
+    public:
+        AmarokPlayer(QObject* parent = 0);
+        virtual void play(const QString& url);
+        virtual void stop();
+
+    private slots:
+        virtual void processPlaylist();
+
+    private:
+        QDBusInterface *playerInterface,
+            *tracklistInterface;
+};
+
+}}
+
+#endif
